fd8a0e85d2a673ca6995587c2983913fc227cfeb
Bryan Maaño <louiebryanmaano@gmail.com>
2025-02-21 21:52:59 +0800

    page added

--- codefolio/src/Sections/Home.jsx
+++ codefolio/src/Sections/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
                     <p>Hello, I'm</p>
                     <h2>Bryan Maaño</h2>
                     <p>
-                        <span data-text1="Aspiring Web Developer" data-text2="Software Engineer">Aspiring Web Developer</span>
+                        <span data-text1="Aspiring Web Developer" data-text2="Front-end developer">Aspiring Web Developer</span>
                     </p>
                     <p className={styles.desc2}>A motivated and passionate individual with a strong interest in web development. Eager to learn, grow, and apply my skills to build user-friendly and innovative web solutions.</p>
                 </div>
--- codefolio/src/Sections/Projects.jsx
+++ codefolio/src/Sections/Projects.jsx
@@ -3,8 +3,76 @@ import styles from './Projects.module.css';
 function Projects() {
     return (
         <section id="Projects" className={styles.section}>
-            <h2>Welcome to Codefolio</h2>
-            <p>This is the home section.</p>
+            <div>
+                <div className={styles.frame}>
+                <div className={styles.container}>
+                    <div className={styles.box}>
+                        <a href="https://mylink-link.com">
+                            <img src="/images/Proj1.png" alt="proj1" />
+                        </a>
+                        <h2>Blaze n' Bite</h2>
+                        <br></br>
+                        <p>Blaze n' Bite is my first personal project, a static restaurant website built with HTML, CSS, and JavaScript. Throughout development, I faced challenges, especially with responsive design and clean coding. These obstacles helped me grow, and by the end, I knew where to focus my efforts for future projects, confident I’d apply the lessons learned to overcome new challenges.</p>
+                    </div>
+                    <div className={styles.box}>
+                        <a href="#">
+                            <img src="/images/Placeholder.jpg" alt="proj1" />
+                        </a>
+                        <h2>Coming Soon</h2>
+                        <br></br>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem odit asperiores odio nesciunt tempora mollitia distinctio repellat vero quod enim? Laborum ad accusamus voluptates esse, explicabo ducimus placeat perspiciatis. Cum.</p>
+                    </div>
+                    <div className={styles.box}>
+                        <a href="https://mylink-link.com">
+                            <img src="/images/Placeholder.jpg" alt="proj1" />
+                        </a>
+                        <h2>Coming Soon</h2>
+                        <br></br>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem odit asperiores odio nesciunt tempora mollitia distinctio repellat vero quod enim? Laborum ad accusamus voluptates esse, explicabo ducimus placeat perspiciatis. Cum.</p>
+                    </div>
+                    <div className={styles.box}>
+                        <a href="https://mylink-link.com">
+                            <img src="/images/Placeholder.jpg" alt="proj1" />
+                        </a>
+                        <h2>Coming Soon</h2>
+                        <br></br>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem odit asperiores odio nesciunt tempora mollitia distinctio repellat vero quod enim? Laborum ad accusamus voluptates esse, explicabo ducimus placeat perspiciatis. Cum.</p>
+                    </div>
+                    <div className={styles.box}>
+                        <a href="https://mylink-link.com">
+                            <img src="/images/Placeholder.jpg" alt="proj1" />
+                        </a>
+                        <h2>Coming Soon</h2>
+                        <br></br>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem odit asperiores odio nesciunt tempora mollitia distinctio repellat vero quod enim? Laborum ad accusamus voluptates esse, explicabo ducimus placeat perspiciatis. Cum.</p>
+                    </div>
+                    <div className={styles.box}>
+                        <a href="https://mylink-link.com">
+                            <img src="/images/Placeholder.jpg" alt="proj1" />
+                        </a>
+                        <h2>Coming Soon</h2>
+                        <br></br>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem odit asperiores odio nesciunt tempora mollitia distinctio repellat vero quod enim? Laborum ad accusamus voluptates esse, explicabo ducimus placeat perspiciatis. Cum.</p>
+                    </div>
+                    <div className={styles.box}>
+                        <a href="https://mylink-link.com">
+                            <img src="/images/Placeholder.jpg" alt="proj1" />
+                        </a>
+                        <h2>Coming Soon</h2>
+                        <br></br>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem odit asperiores odio nesciunt tempora mollitia distinctio repellat vero quod enim? Laborum ad accusamus voluptates esse, explicabo ducimus placeat perspiciatis. Cum.</p>
+                    </div>
+                    <div className={styles.box}>
+                        <a href="https://mylink-link.com">
+                            <img src="/images/Placeholder.jpg" alt="proj1" />
+                        </a>
+                        <h2>Coming Soon</h2>
+                        <br></br>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem odit asperiores odio nesciunt tempora mollitia distinctio repellat vero quod enim? Laborum ad accusamus voluptates esse, explicabo ducimus placeat perspiciatis. Cum.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
         </section>
     );
 }
